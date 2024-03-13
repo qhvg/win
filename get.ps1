bc623095e8a2e644f522576b9ea2513bdc919573
qhvg <99967599+qhvg@users.noreply.github.com>
2024-03-13 19:25:49 +0100

    get.ps1: Script is now downloaded to %Temp%/$env:TEMP

--- get.ps1
+++ get.ps1
@@ -1,3 +1,3 @@
-Invoke-WebRequest -Uri https://qhvg.github.io/win/main.bat -OutFile ".\windows.bat"
-Start-Process -FilePath ".\windows.bat" -Wait
+Invoke-WebRequest -Uri https://qhvg.github.io/win/main.bat -OutFile "$env:TEMP\windows.bat"
+Start-Process -FilePath "$env:TEMP\windows.bat" -Wait
 Remove-Item -Path "windows.bat" -Force
