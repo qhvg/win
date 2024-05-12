e9b1c2e63b8928bd9f4e2b40f961b5a911e9171c
maciek <99967599+qhvg@users.noreply.github.com>
2024-05-12 08:30:36 +0200

    main.bat: MAS fix

--- main.bat
+++ main.bat
@@ -60,7 +60,7 @@ rem ==============================================
 :mas
 curl https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/All-In-One-Version/MAS_AIO-CRC32_9AE8AFBA.cmd --output %Temp%\MAS.cmd
 call %Temp%\MAS.cmd
-echo If finished, press any key to continue.
+pause
 del %Temp%\MAS.cmd
 goto main
 
